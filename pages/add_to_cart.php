1f881a7b9b56ccab20fe639ae7bb81b4c9ea0de9
Trung-Kien Nguyen <90755943+trungkiennguyenn@users.noreply.github.com>
2025-09-08 11:18:20 +0200

    Version v0.01

--- pages/add_to_cart.php
+++ pages/add_to_cart.php
@@ -12,10 +12,11 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 }
 
 // Haal en valideer POST-waarden
-$category   = $_POST['category'] ?? '';
-$id         = isset($_POST['id']) ? (int) $_POST['id'] : 0;
-$quantity   = isset($_POST['quantity']) ? (int) $_POST['quantity'] : 1;
-if ($quantity < 1) $quantity = 1;
+$category = $_POST['category'] ?? '';
+$id = isset($_POST['id']) ? (int) $_POST['id'] : 0;
+$quantity = isset($_POST['quantity']) ? (int) $_POST['quantity'] : 1;
+if ($quantity < 1)
+    $quantity = 1;
 
 $allowedTables = ['laptops', 'smartphones', 'televisies', 'tablets'];
 if (!in_array($category, $allowedTables, true) || $id <= 0) {
@@ -42,11 +43,11 @@ if (isset($_SESSION['cart'][$key])) {
     $_SESSION['cart'][$key]['quantity'] += $quantity;
 } else {
     $_SESSION['cart'][$key] = [
-        'id'        => (int) $product['id'],
-        'category'  => $category,
-        'name'      => $product['name'],
-        'price'     => (float) $product['price'],
-        'quantity'  => $quantity,
+        'id' => (int) $product['id'],
+        'category' => $category,
+        'name' => $product['name'],
+        'price' => (float) $product['price'],
+        'quantity' => $quantity,
         'image_url' => $product['image_url']
     ];
 }
